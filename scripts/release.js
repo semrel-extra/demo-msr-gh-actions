ecc82cc7016936aeae2f28923cdd3b388ff9806a
Anton Golub <mailbox@antongolub.ru>
2019-03-16 15:03:02 +0300

    perf(release): execa hook polishing

--- scripts/release.js
+++ scripts/release.js
@@ -1,34 +1,24 @@
 const execa = require('execa')
 const hook = require('require-in-the-middle')
-const { sync, stdout, stderr, shell, shellSync } = execa
-require('debug').enable('semantic-release:*')
 
 const _execa = (...args) => {
   const result =  new Promise((resolve, reject) => {
     try {
-      resolve(sync(...args))
+      resolve(execa.sync(...args))
     } catch (e) {
       reject(e)
     }
   })
 
-
-  result.stdout = new String(result.stdout || '')
-  result.stderr = new String(result.stderr || '')
-
+  result.stdout = new String('')
+  result.stderr = new String('')
   result.stdout.pipe = () => {}
   result.stderr.pipe = () => {}
 
   return result
 }
 
-
 Object.assign(_execa, execa)
-_execa.sync = sync
-_execa.stdout = stdout
-_execa.stderr = stderr
-_execa.shell = shell
-_execa.shellSync = shellSync
 
 delete require.cache[require.resolve('execa')]
 
